0b597dd29bdc938f3260c7a83b51f096642e22f7
Badminton Open Source Team <team@badmintonfinder.org>
2026-09-21 14:40:55 -0400

    fix: downgrade vite to v5 to fix MUI icons CJS interop bug that caused React #130

---
+++ test-error.js
@@ -0,0 +1,21 @@
+const { chromium } = require('@playwright/test');
+
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  
+  page.on('console', msg => {
+    if (msg.type() === 'error') {
+      console.log('BROWSER ERROR:', msg.text());
+    }
+  });
+
+  page.on('pageerror', err => {
+    console.log('PAGE ERROR:', err.message);
+  });
+
+  await page.goto('http://localhost:3002');
+  await page.waitForTimeout(2000);
+  
+  await browser.close();
+})();
--- web/src/components/BadmintonIcon.tsx
+++ web/src/components/BadmintonIcon.tsx
@@ -1,3 +1,4 @@
+
 import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon';
 
 export function BadmintonIcon(props: SvgIconProps) {
